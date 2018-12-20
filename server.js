d40e340601f11dee98b2a0401ee0d59bd34ce4c9
Paul Bradley <bradleypauld@gmail.com>
2018-12-19 22:12:01 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3002;
 
 app.use(express.static('dist'));
 
-app.listen(port);
+app.listen(port,() => console.log(`Reddit Pictures listening on ${port}`);
